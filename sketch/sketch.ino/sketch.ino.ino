5259ad97b0e9cd63640a505008da22725d282642
Gora <GoraE@stu.uticak12.org>
2020-11-02 13:41:04 -0500

    Roughly implemented JavaScript

---
+++ sketch/sketch.ino/sketch.ino.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include "Adafruit_TCS34725.h" //https://github.com/adafruit/Adafruit_TCS34725/releases/tag/1.3.3
+
+#define redpin 3
+#define greenpin 5
+#define bluepin 6
+
+//Adafruit library configuration
+#define commonAnode true 
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+byte gammatable[256]; // 
+
+String foo = "Bar";
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  Serial.println(foo);
+}
