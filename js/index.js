b1879e4ab26664f43e52abe29f4317764e55db39
Gora <GoraE@stu.uticak12.org>
2020-11-02 13:49:29 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,45 +1,21 @@
+//Libraries
 const delay = require('delay');
-const tmi = require('tmi.js');
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 
+//Declare arduino's COM port and init limiter
 const port = new SerialPort('insertComPortHere', { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
+//Confirms that the COM port is correct
 port.on("open", () => {
   console.log('Serial port open.');
 });
+
+//Reads data via serial
 parser.on('data', data =>{
   console.log('Data recieved from arduino:', data);
 });
 
-const options = {
-  options: {
-    debug: true,
-  },
-  connection: {
-    cluster: 'aws',
-    reconnect: true,
-  },
-  identity: {
-    username: 'insertReaderAccountHere',
-    password: 'insertReaderAccountPasswordHere'
-  },
-  channels: ['insertYourChannelHere'],
-};
 
-const client = new tmi.client(options);
 
-client.connect();
-
-client.on("chat", (channel, userstate, message, self) => {
-    if (message === '!water' && userstate['mod'] === true){
-      console.log('New donator');
-
-      (async () => {
-          port.write('1');
-          await delay(1500);
-          port.write('0');
-      })();
-    }
-
-});
