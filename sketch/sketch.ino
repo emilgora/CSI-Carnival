053b972031a061590444e597b339192c4188f98e
emilgora <30513364+emilgora@users.noreply.github.com>
2020-11-05 12:09:03 -0500

    Added HTML

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,12 +1,7 @@
 #include <Wire.h>
 #include "Adafruit_TCS34725.h" //https://github.com/adafruit/Adafruit_TCS34725/releases/tag/1.3.3
 
-#define redpin 3
-#define greenpin 5
-#define bluepin 6
-
-//Adafruit library configuration
-#define commonAnode true 
+//Adafruit library constructor
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 
 byte gammatable[256]; // 
