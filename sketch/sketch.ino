3737383dfbb7c25f257f6eac0d832e81a335b570
emilgora <30513364+emilgora@users.noreply.github.com>
2020-12-10 22:16:36 -0500

    Did everything lol

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,84 +1,72 @@
-// Libraries included
+// Libraries that we used:
 #include <Wire.h>
 #include "Adafruit_TCS34725.h" //https://github.com/adafruit/Adafruit_TCS34725/releases/tag/1.3.3
 
-// Sorting solenoids pins
-const int solenoidA = 1;
-const int solenoidB = 1;
+// Super-duper secret controls (reset button is wired to the built-in RST)
+const int startButton = 18;
+bool gameState = false;
 
-// Release solenoids pins
-const int solenoidX = 1;
-const int solenoidY = 1;
+// LDR - Will
+const int ldrPin = 9;
+// Data
+int brightness = 0;
+bool ballPresent = false;
 
-// Ultrasonic pins
-#define echoPin 8
-#define trigPin 9
-// Ultrasonic processed data
-float duration, distance; 
-bool ballPresent = true;
-
-//Adafruit color sensor library constructor
+//Adafruit color sensor library constructor - Emil
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
+// Data
 byte gammatable[256]; // 
 
-
-const int motor = 9;
+// Rods - Jacob
+const int rodLeft = 7;
+const int rodRight = 6;
+// Data
 int mot = 0; //# of times arduino detects motor
 int rot = 0; //# of rotations
 int val;
-const int foo = 8;//connect to red led or rgb pin
-const int bar = 7;//connect to other led or rgb pin
+
+// Sorting solenoids
+const int solenoidA = 15;
+const int solenoidB = 14;
+// Release solenoids
+const int solenoidX = 16;
+const int solenoidY = 10;
 
 void setup() {
-  pinMode(trigPin, OUTPUT); 
-  pinMode(echoPin, INPUT); 
+  pinMode(startButton, INPUT);
 
-  pinMode(motor, INPUT);
-  pinMode(foo, OUTPUT);
-  pinMode(bar, OUTPUT);
+  // The LDR doesn't use pinMode because it's uses the built-in analog converter.
+  // The color sensor also doesn't because it's over the I2C bus. 
+
+  pinMode(rodLeft, INPUT);
+  pinMode(rodRight, INPUT);
   
+  pinMode(solenoidA, OUTPUT);
+  pinMode(solenoidB, OUTPUT);
+  pinMode(solenoidX, OUTPUT);
+  pinMode(solenoidY, OUTPUT);
+
+  // Starts the serial monitor for output data.
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(foo);
-
-  // Ultrasonic example sketch (https://create.arduino.cc/projecthub/Isaac100/getting-started-with-the-hc-sr04-ultrasonic-sensor-036380)
-  // Clears the trigPin condition
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(10);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  // Displays the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-
-
-    val = digitalRead(motor);
+  
+  brightness = analogRead(ldrPin);
+  
+  val = digitalRead(rodLeft);
   if(val == HIGH){    //counts how many times motor is detected
     mot = mot+1;
                       //tests to see if motor is detected
-    digitalWrite(foo,HIGH);
-    delay(1000);
-    digitalWrite(foo,LOW);
   }
   if(mot == 4){       //counts number of rotations motor makes, change number in if condition to number or steps motor has
     rot = rot + 1;
     mot = 0;
                       //tests to see if rotation is counted  
-    digitalWrite(bar,HIGH);
-    delay(1000);
-    digitalWrite(bar,LOW);
   }
-
-  /*
+  
+  
+  
   // Color sense example sketch (https://github.com/adafruit/Adafruit_TCS34725/blob/master/examples/tcs34725/tcs34725.ino)
   uint16_t r, g, b, c, colorTemp, lux;
   tcs.getRawData(&r, &g, &b, &c);
@@ -89,9 +77,11 @@ void loop() {
   Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
   Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
   Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
-  Serial.println(" ");
-  */
+  
+
+  
+
+  Serial.print(" ");
+  Serial.println("<- this is the red value");
+  
 }
