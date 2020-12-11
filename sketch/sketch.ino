83cce1ce1fe02d532f3cbc073c9956dcb919f9d1
emilgora <30513364+emilgora@users.noreply.github.com>
2020-12-10 22:48:03 -0500

    very important!!!

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -2,10 +2,6 @@
 #include <Wire.h>
 #include "Adafruit_TCS34725.h" //https://github.com/adafruit/Adafruit_TCS34725/releases/tag/1.3.3
 
-// Super-duper secret controls (reset button is wired to the built-in RST)
-const int startButton = 18;
-bool gameState = false;
-
 // LDR - Will
 const int ldrPin = 9;
 // Data
@@ -21,9 +17,12 @@ byte gammatable[256]; //
 const int rodLeft = 7;
 const int rodRight = 6;
 // Data
-int mot = 0; //# of times arduino detects motor
-int rot = 0; //# of rotations
-int val;
+int motLeft = 0; // # of 
+int motRight = 0; // # of 
+int rotLeft = 0; // # of 
+int rotRight = 0; // # of 
+int valLeft; // # of times the left rod had rotated
+int valRight; // # of 
 
 // Sorting solenoids
 const int solenoidA = 15;
@@ -33,14 +32,13 @@ const int solenoidX = 16;
 const int solenoidY = 10;
 
 void setup() {
-  pinMode(startButton, INPUT);
 
   // The LDR doesn't use pinMode because it's uses the built-in analog converter.
   // The color sensor also doesn't because it's over the I2C bus. 
 
   pinMode(rodLeft, INPUT);
   pinMode(rodRight, INPUT);
-  
+
   pinMode(solenoidA, OUTPUT);
   pinMode(solenoidB, OUTPUT);
   pinMode(solenoidX, OUTPUT);
@@ -54,18 +52,24 @@ void loop() {
   
   brightness = analogRead(ldrPin);
   
-  val = digitalRead(rodLeft);
-  if(val == HIGH){    //counts how many times motor is detected
-    mot = mot+1;
-                      //tests to see if motor is detected
+  valLeft = digitalRead(rodLeft);
+  valRight = digitalRead(rodRight);
+  
+  if(valLeft == HIGH){    // Counts how many times motor is detected
+    motLeft = motLeft+1; // Tests to see if motor is detected      
   }
-  if(mot == 4){       //counts number of rotations motor makes, change number in if condition to number or steps motor has
-    rot = rot + 1;
-    mot = 0;
-                      //tests to see if rotation is counted  
+  if(motLeft == 2){       // Counts number of rotations motor makes, change number in if condition to number or steps motor has
+    rotLeft = rotLeft + 1;
+    motLeft = 0;  // Tests to see if rotation is counted  
   }
   
-  
+    if(valRight == HIGH){    // Counts how many times motor is detected
+    motRight = motRight+1; // Tests to see if motor is detected      
+  }
+  if(motRight == 2){       // Counts number of rotations motor makes, change number in if condition to number or steps motor has
+    rotRight = rotRight + 1;
+    motRight = 0;  // Tests to see if rotation is counted  
+  }
   
   // Color sense example sketch (https://github.com/adafruit/Adafruit_TCS34725/blob/master/examples/tcs34725/tcs34725.ino)
   uint16_t r, g, b, c, colorTemp, lux;
@@ -73,15 +77,11 @@ void loop() {
   // colorTemp = tcs.calculateColorTemperature(r, g, b);
   colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
   lux = tcs.calculateLux(r, g, b);
-  // Output color values to Serial monitor
+  // Output color valLeftues to Serial monitor
   Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
   Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
   Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
   
-
-  
-
-  Serial.print(" ");
-  Serial.println("<- this is the red value");
+  Serial.println("<- this is the red valLeftue");
   
 }
